152da56ac2d87b8c6f8ebc43415a93c575adc236
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-01-17 17:02:03 +0900

    변경 인원 최신 데이터만 유지
    * 변경 인원 최신 데이터만 유지
    
    - 방 현재 인원이 변경될 경우 최신 데이터만 유지하도록 변경
    
    * 미사용 로그 제거 및 포맷팅

--- src/main/java/com/example/quiz/controller/GameController.java
+++ src/main/java/com/example/quiz/controller/GameController.java
@@ -25,17 +25,8 @@ public class GameController {
 
     @MessageMapping("/{id}/ready")
     public void ready(@DestinationVariable String id, RequestUserId requestUserId) {
-        log.info("user ID is {}", requestUserId.userId());
         ResponseMessage responseMessage = gameService.toggleReadyStatus(id, requestUserId.userId());
 
-        log.info("id : {} role : {}, userId : {}, readyStatus : {}, email : {}, allReadyStatus : {}" ,
-                id,
-                responseMessage.role(),
-                responseMessage.userId(),
-                responseMessage.readyStatus(),
-                responseMessage.email(),
-                responseMessage.allReadyStatus()
-        );
         messagingTemplate.convertAndSend("/pub/room/"+id, responseMessage);
     }
 
@@ -49,7 +40,6 @@ public class GameController {
 
     @MessageMapping("/{id}/send")
     public void sendQuiz(@DestinationVariable String id, RequestUserInfoAnswer userInfoAnswer){
-        log.info("응답이 들어왔습니다");
         ResponseQuiz responseQuiz = gameService.sendQuiz(id,userInfoAnswer);
 
         messagingTemplate.convertAndSend("/pub/"+id+"/send", responseQuiz);
@@ -57,7 +47,6 @@ public class GameController {
 
     @MessageMapping("/{id}/check")
     public void checkQuiz(@DestinationVariable String id, RequestAnswer requestAnswer){
-        log.info("응답");
         ResponseQuiz responseQuiz = gameService.checkAnswer(id, requestAnswer);
 
         messagingTemplate.convertAndSend("/pub/"+id+"/check",responseQuiz);
