deb8c1c289357cde3b5ee0fe158870a554b031be
Minkey99 <songkc5671@naver.com>
2025-01-20 17:30:26 +0900

    DTO 반환 데이터 수정
    * ResponseCheckQuiz : 현재 라운드 승리자가 나왔는지 유무의 currentResult와 전체 라운드 승리자가 나왔는지 유무의 finalResult, 최종 우승자를 finalWinners를 List<> 형으로 반환합니다.
    * ResponseQuiz : 답을 못 맞췄을 경우도 답과 설명을 전달해야하기 때문에 정답과 설명을 문제 생성했을때도 전달함

--- src/main/java/com/example/quiz/dto/response/ResponseCheckQuiz.java
+++ src/main/java/com/example/quiz/dto/response/ResponseCheckQuiz.java
@@ -1,4 +1,6 @@
 package com.example.quiz.dto.response;
 
-public record ResponseCheckQuiz(String email, Boolean result, String correctAnswer, String description) {
+import java.util.List;
+
+public record ResponseCheckQuiz(String email, Boolean currentResult, Boolean finalResult, List<String> finalWinners, String correctAnswer, String description) {
 }
